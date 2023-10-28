3bf3526c4ce33bfcbe634ff86e61ff61a6af3c6b
Jean Rojas <jeanrojas2007@outlook.com>
2023-10-28 04:44:54 -0300

    add postgres in container

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { UsuarioModule } from './usuario/usuario.module';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    UsuarioModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
