37410ee7545244820d91049cd9f996fc299f3ee4
bluexbaez <bluexbaez@gmail.com>
2023-12-05 21:19:22 -0300

    a

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
 
 variable "tcb_blog_vpc_id" {
-  default = "vpc-07d53184d4ab59e23" # Orientações para copia da VPC ID abaixo.
+  default = "vpc-029202a1ccd653a87" # Orientações para copia da VPC ID abaixo.
 }
 
 variable "tcb_blog_subnet_public_id" {
-  default = "subnet-0ac27896134f33555" # Orientações para copia da Subnet ID abaixo.
+  default = "subnet-007701e5253d17370" # Orientações para copia da Subnet ID abaixo.
 }
